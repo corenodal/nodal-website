d41302a42e521ed92da2fdff70c3376bbc1ad911
mandarangchekar <mandarangchekar7@gmail.com>
2026-03-21 11:30:25 -0500

    small fix

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,11 +1,20 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // Kill all ScrollTrigger instances from the previous page
+    ScrollTrigger.getAll().forEach(t => t.kill());
+    // Reset scroll position
     window.scrollTo(0, 0);
+    // Refresh ScrollTrigger after new page mounts
+    const timer = setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 100);
+    return () => clearTimeout(timer);
   }, [pathname]);
 
   return null;
